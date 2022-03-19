0e2805f0476227732a1c33c20df0bae089e1c847
sagheer <mohammed.sagheer@techwondoe.com>
2022-03-19 15:28:27 +0530

    added edit option

--- index.js
+++ index.js
@@ -21,16 +21,29 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 
-app.listen(3000, () => console.log("Server up"))
+
 
 app.get('/',(req, res) => {
-    db.select("*").from("todo").then(data =>{
+    db.select("*").from("todo").orderBy("id", "desc").then(data =>{
         res.render("todo.ejs", { todos: data })
     });
     
 })
 
 app.post('/', (req, res) => {
-    console.log(req.body);
-})
+    console.log(req.body.content);
+    db("todo").insert({"task": req.body.content})
+    .then(_ => res.redirect("/"));
+});
 
+
+app.put("/moveTaskDone", (req, res) => {
+    console.log(req.body);
+    
+    db("todo").where("id","=",req.body.id).update("status", 1).returning("status").then(_ => res.json({"id":req.body.id}));
+   
+    
+});
+
+
+app.listen(3000, () => console.log("Server up"))
